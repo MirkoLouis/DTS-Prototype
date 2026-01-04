88e7fcd8cb3534fbd260e568b04833b93b5408da
Mark Leo Bagood <mirkolouis33@gmail.com>
2026-01-04 23:16:20 +0800

    Defaulting routings from http to https.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,41 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
-
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
-
 {
-
     /**
-
      * Register any application services.
-
      */
-
     public function register(): void
-
     {
-
         //
-
     }
-
-
 
     /**
-
      * Bootstrap any application services.
-
      */
-
     public function boot(): void
-
     {
-
-        //
-
+        // Force HTTPS for all generated links
+        if (config('app.env') !== 'production') {
+            URL::forceScheme('https');
+        }
     }
-
 }
